81d50e3ea9ab88172e38f5fa1ac3790e074affaa
meliqa <test@meliqa.du>
2019-08-30 18:49:09 +0400

    057 Coding Challenge 6 Solution Part 3

--- PigGame/app.js
+++ PigGame/app.js
@@ -1,9 +1,10 @@
-var scores, roundScore, activePlayer, gamePlayng, prevDice, maxScore, p1, p2;
+var scores, roundScore, activePlayer, gamePlayng, prevDice, maxScore, p1, p2, dices;
 init();
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+
     maxScore = document.getElementById('scoreinput').value;
     p1 = document.getElementById('input-p-0').value;
     p2 = document.getElementById('input-p-1').value;
@@ -18,13 +19,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         }
 
         var dice = Math.floor(Math.random() * 6) + 1;
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'Dice-' + dice + '.png';
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        dices = dice + dice2;
 
-        if(dice !== 1){
-            roundScore += dice;
+        var diceDOM = document.querySelector('.dice');
+        var diceDOM2 = document.querySelector('.dice2');
+
+        diceDOM.style.display = 'block';
+        diceDOM2.style.display = 'block';
+
+        diceDOM.src = 'Dice-' + dice + '.png';
+        diceDOM2.src = 'Dice-' + dice2 + '.png';
+
+        if(dice !== 1 && dice2 !== 1){
+            roundScore += dices;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
             if(dice == 6){
                 if(dice == prevDice){
                     scores[activePlayer] = 0;
@@ -37,6 +47,20 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             else if(dice !== 6){
                 prevDice = 0;
             }
+
+            if(dice2 == 6){
+                if(dice2 == prevDice){
+                    scores[activePlayer] = 0;
+                    document.querySelector('#score-' + activePlayer).textContent = 0;
+                    nextPlayer();
+                }else{
+                    prevDice = dice2;
+                }
+            }
+            else if(dice2 !== 6){
+                prevDice = 0;
+            }
+
         }else{
             nextPlayer();
         }
@@ -63,13 +87,14 @@ document.querySelector('.btn-new').addEventListener('click', init)
 
 function nextPlayer(){
 
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        prevDice = 0;
-        document.querySelector('#current-0').textContent = 0;
-        document.querySelector('#current-1').textContent = 0;
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    prevDice = 0;
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
 //        document.querySelector('.dice').style.display = 'none';
 
 //      document.querySelector('.player-0-panel').classList.remove('active');
@@ -83,6 +108,7 @@ function init(){
     activePlayer = 0;
     maxScore = 0;
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
